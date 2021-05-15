b4fb6a33956850e37d840704cf69a390afbc55ed
1552980358 <1552980358@qq.com>
2021-05-15 19:18:07 +0800

    Update GPUBurnerTask::prepare()
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- gpu_burner/GPUBurnerTask.cpp
+++ gpu_burner/GPUBurnerTask.cpp
@@ -1,7 +1,7 @@
 #include "GPUBurnerTask.h"
 
 void GPUBurnerTask::prepare() {
-
+    _start_milliseconds = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
 }
 
 void GPUBurnerTask::burn() {
