555148d58b2cd4fce84aa71b9d9f2658926466ba
1552980358 <1552980358@qq.com>
2021-05-18 14:37:38 +0800

    GPUBurner: Remove redundant methods
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- GPUBurner.cpp
+++ GPUBurner.cpp
@@ -53,53 +53,12 @@ void GPUBurner::prepare() {
     getchar();
 }
 
-struct pi_double_struct {
-    double pi{};
-};
-
-auto get_pi_double_structs(const int &size) {
-    vector<pi_double_struct> structs(size);
-    std::generate(structs.begin(), structs.end(), []() {
-        return pi_double_struct{ 0 };
-    });
-    return structs;
-}
-
-BOOST_COMPUTE_ADAPT_STRUCT(pi_double_struct, pi_double_struct, (pi));
-
-BOOST_COMPUTE_FUNCTION(double, get_pi_double, (pi_double_struct var_pi_struct), {
-            for (int i = 1; i < INT_MAX; ++i) {
-                var_pi_struct.pi += (double) ((i % 2) ? 1 : -1) * (1 / (2 * (double) i + 1));
-            }
-            return var_pi_struct.pi;
-        });
-
-void cal_pi_double(boost::compute::context &context, boost::compute::command_queue &queue, const int compute_unit) {
-    vector<pi_double_struct> cpu_structs = get_pi_double_structs(compute_unit);
-    boost::compute::vector<pi_double_struct> gpu_structs(cpu_structs.size(), context);
-    boost::compute::copy(cpu_structs.begin(), cpu_structs.end(), gpu_structs.begin(), queue);
-    boost::compute::vector<double> gpu_pi(cpu_structs.size(), context);
-    boost::compute::transform(gpu_structs.begin(), gpu_structs.end(), gpu_pi.begin(), get_pi_double, queue);
-    vector<double> cpu_pi(gpu_pi.size());
-    boost::compute::copy(gpu_pi.begin(), gpu_pi.end(), cpu_pi.begin(), queue);
-}
-
 void GPUBurner::burn() {
 
     if (_input != "YES") {
         return;
     }
 
-    boost::compute::context context(_device);
-    boost::compute::command_queue queue(context, _device);
-
-    milliseconds milliseconds_start = duration_cast< milliseconds >(system_clock::now().time_since_epoch());
-
-    cal_pi_double(context, queue, (int) _device.compute_units());
-
-    milliseconds milliseconds_stop = duration_cast< milliseconds >(system_clock::now().time_since_epoch());
-
-    cout << "Time spend: " << milliseconds_stop.count() - milliseconds_start.count() << " ms." << endl;
 
 }
 
